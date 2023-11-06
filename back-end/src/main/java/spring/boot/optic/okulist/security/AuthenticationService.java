ce7778649ece1a8d53c9057e28cdc8c867d9e5d5
Gleb Chebotarov <taronassvolt@gmail.com>
2023-11-06 22:38:50 +0200

    add Category mapper

--- back-end/src/main/java/spring/boot/optic/okulist/security/AuthenticationService.java
+++ back-end/src/main/java/spring/boot/optic/okulist/security/AuthenticationService.java
@@ -1,7 +1,6 @@
 package spring.boot.optic.okulist.security;
 
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
