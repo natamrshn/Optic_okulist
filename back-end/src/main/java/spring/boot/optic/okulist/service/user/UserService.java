e25c591d425522f7ffdc6bbf0141b7a8e625b9c0
Ros <traine41@icloud.com>
2023-11-07 16:21:27 +0000

    User service interface and his implementation

---
+++ back-end/src/main/java/spring/boot/optic/okulist/service/user/UserService.java
@@ -0,0 +1,12 @@
+package spring.boot.optic.okulist.service.user;
+
+import spring.boot.optic.okulist.dto.user.UserRegistrationRequestDto;
+import spring.boot.optic.okulist.dto.user.UserResponseDto;
+import spring.boot.optic.okulist.exception.RegistrationException;
+import spring.boot.optic.okulist.model.User;
+
+public interface UserService {
+    UserResponseDto register(UserRegistrationRequestDto requestDto) throws RegistrationException;
+
+    User getAuthenticated();
+}
