7cfa386f43ce617f11276f7d8b474eb2632a4668
Ros <traine41@icloud.com>
2023-11-07 16:20:20 +0000

    Modified CustomUserDetailsService.
    Was added loadUserByUsername method

--- back-end/src/main/java/spring/boot/optic/okulist/security/CustomUserDetailsService.java
+++ back-end/src/main/java/spring/boot/optic/okulist/security/CustomUserDetailsService.java
@@ -1,4 +1,21 @@
 package spring.boot.optic.okulist.security;
 
-public class CustomUserDetailsService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import spring.boot.optic.okulist.repository.UserRepository;
+
+@RequiredArgsConstructor
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new RuntimeException("Can't find user by email!"));
+    }
 }
+
