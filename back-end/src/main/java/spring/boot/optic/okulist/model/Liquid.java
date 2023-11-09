909320a1f9f4fd158872d9d5d8cb4ea7102aeb66
Ros <traine41@icloud.com>
2023-11-09 18:33:14 +0000

    Model classes were created

---
+++ back-end/src/main/java/spring/boot/optic/okulist/model/Liquid.java
@@ -0,0 +1,23 @@
+package spring.boot.optic.okulist.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@SQLDelete(sql = "Update liquids SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "liquids")
+public class Liquid extends Product {
+    private int volume;
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
+}
