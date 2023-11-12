c5794f2aca00baeb226253cced3dc0082b2533a1
Ros <traine41@icloud.com>
2023-11-12 23:03:59 +0000

    added new methods

--- back-end/src/main/java/spring/boot/optic/okulist/service/glasses/GlassesServiceImpl.java
+++ back-end/src/main/java/spring/boot/optic/okulist/service/glasses/GlassesServiceImpl.java
@@ -1,30 +1,37 @@
 package spring.boot.optic.okulist.service.glasses;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import spring.boot.optic.okulist.dto.glasses.GlassesRequestDto;
 import spring.boot.optic.okulist.dto.glasses.GlassesResponseDto;
+import spring.boot.optic.okulist.dto.glasses.GlassesSearchParameter;
 import spring.boot.optic.okulist.exception.EntityNotFoundException;
 import spring.boot.optic.okulist.mapper.GlassesMapper;
 import spring.boot.optic.okulist.model.Glasses;
 import spring.boot.optic.okulist.repository.GlassesRepository;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import spring.boot.optic.okulist.specification.glasses.builders.GlassesSpecificationBuilder;
 
 @Service
 @RequiredArgsConstructor
 public class GlassesServiceImpl implements GlassesService {
     private final GlassesRepository glassesRepository;
     private final GlassesMapper glassesMapper;
+    private final GlassesSpecificationBuilder glassesSpecificationBuilder;
 
     @Override
     public List<GlassesResponseDto> findAll(Pageable pageable) {
-            return glassesRepository.findAll()
-                    .stream()
-                    .map(glassesMapper::toDto)
-                    .toList();
+        return glassesRepository.findAll()
+                .stream()
+                .map(glassesMapper::toDto)
+                .toList();
     }
 
     @Override
@@ -44,38 +51,46 @@ public class GlassesServiceImpl implements GlassesService {
     @Override
     public void deleteById(Long id) {
         Glasses glasses = glassesRepository.findById(id).orElseThrow(
-                () ->new EntityNotFoundException("Can't found glasses with ID :" + id)
+                () -> new EntityNotFoundException("Can't found glasses with ID :" + id)
         );
         glasses.setDeleted(true);
         glassesRepository.save(glasses);
     }
 
     @Override
-    public List<GlassesResponseDto> findSimilar(GlassesRequestDto glassesRequestDto) {
-        Glasses referenceGlasses = glassesMapper.toModel(glassesRequestDto);
-
-        List<Glasses> similarGlasses = glassesRepository.findByColorIgnoreCaseAndNameAndPriceAndIdentifierAndDescription(
-                referenceGlasses.getColor(),
-                referenceGlasses.getName(),
-                referenceGlasses.getPrice(),
-                referenceGlasses.getIdentifier(),
-                referenceGlasses.getDescription()
-        );
-
-        return similarGlasses.stream()
+    public List<GlassesResponseDto> searchGlassesByParameters(
+            GlassesSearchParameter searchParameters) {
+        Specification<Glasses> bookSpecification = glassesSpecificationBuilder
+                .build(searchParameters);
+        return glassesRepository.findAll(bookSpecification)
+                .stream()
                 .map(glassesMapper::toDto)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     @Override
     public GlassesResponseDto update(Long id, GlassesRequestDto glassesRequestDto) {
-        Glasses existingGlasses = glassesRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException("Can't find Glasses with ID: " + id)
-        );
+        Glasses existingGlasses = glassesRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Can't find Glasses with ID: " + id));
+        BeanUtils.copyProperties(glassesRequestDto, existingGlasses,
+                getNullPropertyNames(glassesRequestDto));
+        Glasses updatedGlasses = glassesRepository.save(existingGlasses);
+        return glassesMapper.toDto(updatedGlasses);
+    }
 
-        Glasses updatedGlasses = glassesMapper.toModel(glassesRequestDto);
-        updatedGlasses.setId(existingGlasses.getId());
-
-        return glassesMapper.toDto(glassesRepository.save(updatedGlasses));
+    // Допоміжний метод для отримання імен властивостей з null значеннями
+    private String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+        Set<String> emptyNames = new HashSet<>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
     }
 }
