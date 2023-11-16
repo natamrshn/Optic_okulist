9de7be305dad32c2a10a56a4f9ee0eecbb0bc1fa
Ros <traine41@icloud.com>
2023-11-16 15:06:58 +0000

    Material dto

---
+++ back-end/src/main/java/spring/boot/optic/okulist/dto/contactlenses/parameters/material/MaterialRequestDto.java
@@ -0,0 +1,8 @@
+package spring.boot.optic.okulist.dto.contactlenses.parameters.material;
+
+import lombok.Data;
+
+@Data
+public class MaterialRequestDto {
+    private String material;
+}
---
+++ back-end/src/main/java/spring/boot/optic/okulist/dto/contactlenses/parameters/material/MaterialResponseDto.java
@@ -0,0 +1,9 @@
+package spring.boot.optic.okulist.dto.contactlenses.parameters.material;
+
+import lombok.Data;
+
+@Data
+public class MaterialResponseDto {
+    private Long id;
+    private String material;
+}
