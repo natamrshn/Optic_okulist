7cb9c126b868fe283713bc2f7aec157a9e130b50
Ros <traine41@icloud.com>
2023-11-16 14:18:23 +0000

    Deleted unnecessary classes

--- back-end/src/main/java/spring/boot/optic/okulist/specification/contactlenses/managers/SpecificationProviderManagerLenses.java
+++
@@ -1,7 +0,0 @@
-package spring.boot.optic.okulist.specification.contactlenses.managers;
-
-import spring.boot.optic.okulist.specification.contactlenses.providers.SpecificationProviderLenses;
-
-public interface SpecificationProviderManagerLenses<T> {
-    SpecificationProviderLenses<T> getSpecificationProvider(String key);
-}
--- back-end/src/main/java/spring/boot/optic/okulist/specification/contactlenses/providers/SpecificationProviderLenses.java
+++
@@ -1,9 +0,0 @@
-package spring.boot.optic.okulist.specification.contactlenses.providers;
-
-import org.springframework.data.jpa.domain.Specification;
-
-public interface SpecificationProviderLenses<T> {
-    String getKey();
-
-    Specification<T> getContactLensesSpecification(String[] params);
-}
