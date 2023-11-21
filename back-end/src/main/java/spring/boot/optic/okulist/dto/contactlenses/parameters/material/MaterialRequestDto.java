2279550b385a5a56cf8e6b09f78ef16ae14cf11b
Ros <traine41@icloud.com>
2023-11-21 20:57:16 +0000

    deleted unnecessary dto

--- back-end/src/main/java/spring/boot/optic/okulist/dto/contactlenses/parameters/material/MaterialRequestDto.java
+++
@@ -1,8 +0,0 @@
-package spring.boot.optic.okulist.dto.contactlenses.parameters.material;
-
-import lombok.Data;
-
-@Data
-public class MaterialRequestDto {
-    private String material;
-}
--- back-end/src/main/java/spring/boot/optic/okulist/dto/contactlenses/parameters/material/MaterialResponseDto.java
+++
@@ -1,9 +0,0 @@
-package spring.boot.optic.okulist.dto.contactlenses.parameters.material;
-
-import lombok.Data;
-
-@Data
-public class MaterialResponseDto {
-    private Long id;
-    private String material;
-}
