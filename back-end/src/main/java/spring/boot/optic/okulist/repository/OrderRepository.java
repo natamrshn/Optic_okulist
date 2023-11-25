a98563379f2b79f030fd9dedffcda15ab94966e9
Gleb Chebotarov <taronassvolt@gmail.com>
2023-11-25 15:39:58 +0200

    add repository for order

---
+++ back-end/src/main/java/spring/boot/optic/okulist/repository/OrderRepository.java
@@ -0,0 +1,16 @@
+package spring.boot.optic.okulist.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import spring.boot.optic.okulist.model.Order;
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Order findByUserId(Long userId);
+
+    @Query("SELECT o FROM Order o WHERE o.id = :order_id"
+            + " AND EXISTS (SELECT oi FROM o.orderItems oi WHERE oi.id = :order_item_id)")
+    Optional<Order> findByOrderIdAndOrderItemsId(@Param("order_id") Long orderId,
+                                                 @Param("order_item_id") Long orderItemsId);
+}
