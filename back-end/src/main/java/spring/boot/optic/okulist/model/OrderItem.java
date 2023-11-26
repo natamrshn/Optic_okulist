8e9d12f450662101aef89e41d767590f4122f945
Ros <traine41@icloud.com>
2023-11-26 15:17:53 +0000

    added table names for correct work with liquibase

--- back-end/src/main/java/spring/boot/optic/okulist/model/OrderItem.java
+++ back-end/src/main/java/spring/boot/optic/okulist/model/OrderItem.java
@@ -9,11 +9,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "orders_items")
 public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
