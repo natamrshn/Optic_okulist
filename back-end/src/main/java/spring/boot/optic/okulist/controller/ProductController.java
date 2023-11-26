5583209b225f0734169dfb49aeb5220bccc4c92f
Ros <traine41@icloud.com>
2023-11-26 15:17:22 +0000

    fix checkstyle

--- back-end/src/main/java/spring/boot/optic/okulist/controller/ProductController.java
+++ back-end/src/main/java/spring/boot/optic/okulist/controller/ProductController.java
@@ -2,18 +2,24 @@ package spring.boot.optic.okulist.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import spring.boot.optic.okulist.dto.product.ProductRequestDto;
 import spring.boot.optic.okulist.dto.product.ProductResponseDto;
 import spring.boot.optic.okulist.dto.product.ProductSearchParameter;
 import spring.boot.optic.okulist.service.product.ProductService;
-
-import java.util.List;
 
 @Tag(name = "Product Controller",
         description = "Endpoints for managing product")
