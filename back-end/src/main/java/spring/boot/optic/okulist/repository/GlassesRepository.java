77bddaebef9b3d0f74239ff2c16733c68a1f01bb
Ros <traine41@icloud.com>
2023-12-05 15:16:17 +0000

    change Double to BigDecimal

--- back-end/src/main/java/spring/boot/optic/okulist/repository/GlassesRepository.java
+++ back-end/src/main/java/spring/boot/optic/okulist/repository/GlassesRepository.java
@@ -1,5 +1,6 @@
 package spring.boot.optic.okulist.repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,9 +11,7 @@ import spring.boot.optic.okulist.model.Glasses;
 public interface GlassesRepository extends JpaRepository<Glasses, Long> {
     List<Glasses> findAll(Specification<Glasses> specification);
 
-    List<Glasses> findByModelAndIsDeletedFalse(String model);
-
     List<Glasses> findByColorIgnoreCaseAndNameAndPriceAndIdentifierAndDescription(
-            String color, String name, double price, String identifier, String description
+            String color, String name, BigDecimal price, String identifier, String description
     );
 }
