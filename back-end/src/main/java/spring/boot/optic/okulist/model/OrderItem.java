85641e39aaa26cb7a5e2d18ea0642d3a97c65c21
Oleksandr Kovalenko <ol.kovalenko@enamine.net>
2023-12-16 22:16:31 +0200

    fix build

--- back-end/src/main/java/spring/boot/optic/okulist/model/OrderItem.java
+++ back-end/src/main/java/spring/boot/optic/okulist/model/OrderItem.java
@@ -2,8 +2,6 @@ package spring.boot.optic.okulist.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
