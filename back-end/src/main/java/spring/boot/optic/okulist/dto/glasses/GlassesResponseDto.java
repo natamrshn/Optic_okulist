94f8ddbab00fe1c1f1cf2b7706e6d1716305a47f
Oleksandr Kovalenko <ol.kovalenko@enamine.net>
2023-12-23 20:22:45 +0200

    glasses variation init commit

--- back-end/src/main/java/spring/boot/optic/okulist/dto/glasses/GlassesResponseDto.java
+++ back-end/src/main/java/spring/boot/optic/okulist/dto/glasses/GlassesResponseDto.java
@@ -1,9 +1,12 @@
 package spring.boot.optic.okulist.dto.glasses;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Set;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import spring.boot.optic.okulist.dto.category.CategoryResponseDto;
+import spring.boot.optic.okulist.model.Glasses;
 
 @Data
 public class GlassesResponseDto {
@@ -18,4 +21,16 @@ public class GlassesResponseDto {
     private String imageUrl;
     private String imageUrlSecond;
     private Set<CategoryResponseDto> categories;
+    List<Variation> variations;
+
+    @Data
+    @NoArgsConstructor
+    public static class Variation {
+        private Long id;
+        private String coverImage;
+
+        public Variation(Glasses glasses) {
+            this.id = glasses.getId();
+        }
+    }
 }
