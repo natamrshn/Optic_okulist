468d85a903065c5498c69dfe195596a768417f98
Ros <traine41@icloud.com>
2023-12-24 18:04:07 +0000

    Change exception from RuntimeException to UsernameNotFoundException

--- back-end/src/main/java/spring/boot/optic/okulist/security/CustomUserDetailsService.java
+++ back-end/src/main/java/spring/boot/optic/okulist/security/CustomUserDetailsService.java
@@ -15,7 +15,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username)
-                .orElseThrow(() -> new RuntimeException("Can't find user by email!"));
+                .orElseThrow(() -> new UsernameNotFoundException("Can't find user by email!"));
     }
 }
 
