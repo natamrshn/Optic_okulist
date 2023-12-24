5e21157b174d891e5db397e9dad0ffcd492129f3
Ros <traine41@icloud.com>
2023-12-24 17:58:03 +0000

    deleted unecessary classes

--- back-end/src/main/java/spring/boot/optic/okulist/service/TempUserOrderService.java
+++
@@ -1,8 +0,0 @@
-package spring.boot.optic.okulist.service;
-
-import spring.boot.optic.okulist.dto.TempOrderRequestDto;
-import spring.boot.optic.okulist.dto.TempOrderResponseDto;
-
-public interface TempUserOrderService {
-    TempOrderResponseDto processOrder(TempOrderRequestDto orderRequest);
-}
