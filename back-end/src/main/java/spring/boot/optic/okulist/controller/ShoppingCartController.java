4f4b51aca81ad047c97ac3c6802dd30d0ac06e33
Ros <traine41@icloud.com>
2023-12-27 15:52:44 +0000

    impl CookieValue to getShoppingCart controller

--- back-end/src/main/java/spring/boot/optic/okulist/controller/ShoppingCartController.java
+++ back-end/src/main/java/spring/boot/optic/okulist/controller/ShoppingCartController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,15 +13,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import spring.boot.optic.okulist.dto.shoppingcart.ShoppingCartResponseDto;
 import spring.boot.optic.okulist.dto.shoppingcartitems.CartItemResponseDto;
 import spring.boot.optic.okulist.dto.shoppingcartitems.ShoppingCartItemsRequestDto;
 import spring.boot.optic.okulist.dto.shoppingcartitems.UpdateQuantityDto;
-import spring.boot.optic.okulist.exception.AuthenticationException;
+import spring.boot.optic.okulist.model.user.TemporaryUser;
 import spring.boot.optic.okulist.service.cartitem.CartItemService;
 import spring.boot.optic.okulist.service.shoppingcart.ShoppingCartService;
+import spring.boot.optic.okulist.service.user.UserService;
 
 @Tag(name = "Shopping Cart Controller management",
         description = "Endpoints for managing products in Shopping Carts")
@@ -30,6 +31,7 @@ import spring.boot.optic.okulist.service.shoppingcart.ShoppingCartService;
 public class ShoppingCartController {
     private final ShoppingCartService shoppingCartService;
     private final CartItemService cartItemService;
+    private final UserService userService;
 
     @PostMapping
     @Operation(summary = "add new item to a shopping cart")
@@ -40,24 +42,15 @@ public class ShoppingCartController {
 
     @GetMapping
     @Operation(summary = "Get shopping cart")
-    public ShoppingCartResponseDto getShoppingCart(@RequestParam(required = false) String sessionId,
-                                                   //TODO: think of cookies or header instead of RequestParam
+    public ShoppingCartResponseDto getShoppingCart(@CookieValue("sessionId") String sessionId,
                                                    Authentication authentication) {
-        if ( (authentication == null || !authentication.isAuthenticated())
-                && sessionId == null) {
-            throw new AuthenticationException ("User should be authenticated or sessionId provided");
+        if (authentication == null || !authentication.isAuthenticated()) {
+            TemporaryUser temporaryUser = userService.createTemporary(sessionId);
+            sessionId = temporaryUser.getSessionId();
         }
         return shoppingCartService.getShoppingCart(sessionId);
     }
 
-    /*
-        @GetMapping
-    public ShoppingCartResponseDto getShoppingCart(@CookieValue(name = "sessionId", required = false) String sessionId,
-                                                   HttpServletResponse response) {
-        String finalSessionId = resolveSessionId(sessionId);
-        return shoppingCartService.getShoppingCart(finalSessionId);
-    }
-     */
 
     @PutMapping("/cart-items/{id}")
     @Operation(summary = "Update a cart item by ID")
