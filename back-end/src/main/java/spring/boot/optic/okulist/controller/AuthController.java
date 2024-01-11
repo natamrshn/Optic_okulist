b6ffecad69e09c91d1b03348d55e4295dcba3973
Ros <traine41@icloud.com>
2024-01-11 17:35:37 +0000

    created entity dto etc and write controllers

--- back-end/src/main/java/spring/boot/optic/okulist/controller/AuthController.java
+++ back-end/src/main/java/spring/boot/optic/okulist/controller/AuthController.java
@@ -4,18 +4,28 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring.boot.optic.okulist.dto.product.ProductResponseDto;
 import spring.boot.optic.okulist.dto.user.UserLoginRequestDto;
 import spring.boot.optic.okulist.dto.user.UserLoginResponseDto;
 import spring.boot.optic.okulist.dto.user.UserRegistrationRequestDto;
 import spring.boot.optic.okulist.dto.user.UserResponseDto;
+import spring.boot.optic.okulist.exception.AuthenticationException;
 import spring.boot.optic.okulist.exception.RegistrationException;
+import spring.boot.optic.okulist.model.RegisteredUser;
 import spring.boot.optic.okulist.security.AuthenticationService;
 import spring.boot.optic.okulist.service.user.UserService;
 
@@ -44,5 +54,37 @@ public class AuthController {
     @PostMapping("/login")
     public UserLoginResponseDto login(@RequestBody UserLoginRequestDto requestDto) {
         return authenticationService.authenticate(requestDto);
+    }
+
+    @PostMapping("/add-favorite-product/{productId}")
+    public ResponseEntity<ProductResponseDto> addFavouriteProduct(Authentication authentication,
+                                                                  @PathVariable Long productId) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new AuthenticationException("User should be authenticated");
+        }
+        RegisteredUser user = (RegisteredUser) authentication.getPrincipal();
+        ProductResponseDto addedProduct = userService.addFavouriteProduct(user, productId);
+        return new ResponseEntity<>(addedProduct, HttpStatus.OK);
+    }
+
+    @GetMapping("/favorite-products")
+    public ResponseEntity<Set<ProductResponseDto>> getFavoriteProducts(Authentication authentication) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new AuthenticationException("User should be authenticated");
+        }
+        RegisteredUser user = (RegisteredUser) authentication.getPrincipal();
+        Set<ProductResponseDto> favoriteProducts = userService.getFavoriteProducts(user.getId());
+        return new ResponseEntity<>(favoriteProducts, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete-favorite-product/{productId}")
+    public ResponseEntity<Void> deleteFavoriteProduct(Authentication authentication,
+                                                      @PathVariable Long productId) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new AuthenticationException("User should be authenticated");
+        }
+        RegisteredUser user = (RegisteredUser) authentication.getPrincipal();
+        userService.deleteFavoriteProduct(user.getId(), productId);
+        return ResponseEntity.noContent().build();
     }
 }
