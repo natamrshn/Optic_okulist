f78e2e791a81b69ede6484297af4922f32fd7a4b
oleksandrshtonda <oleksandrshtonda@gmail.com>
2024-01-21 11:38:14 +0100

    Added routes, added AuthContext, updated Readme.md, added LocalStorage util and tests for it, installed react-router-dom and classnames libs

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import { MainPage } from '@pages/MainPage';
+import { AboutUsPage } from '@pages/AboutUsPage';
+import { ReviewsPage } from '@pages/ReviewsPage';
+import { ServicesPage } from '@pages/ServicesPage';
+import { UsefulArticlesPage } from '@pages/UsefulArticlesPage';
+import { ContactsPage } from '@pages/ContactsPage';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={ <MainPage /> }/>
+          <Route path="/about-us" element={ <AboutUsPage /> }/>
+          <Route path="/reviews" element={ <ReviewsPage /> }/>
+          <Route path="/services" element={ <ServicesPage /> }/>
+          <Route path="/contacts" element={ <UsefulArticlesPage /> }/>
+          <Route path="/useful-articles" element={ <ContactsPage /> }/>
+          
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
