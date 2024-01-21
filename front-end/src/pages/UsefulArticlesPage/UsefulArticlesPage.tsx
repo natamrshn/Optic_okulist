b69564ebd38d0bd51acadde96d7900849d0548c4
oleksandrshtonda <oleksandrshtonda@gmail.com>
2024-01-21 20:39:18 +0100

    updated favicon in react project, added UI, updated package.json

--- front-end/src/pages/UsefulArticlesPage/UsefulArticlesPage.tsx
+++ front-end/src/pages/UsefulArticlesPage/UsefulArticlesPage.tsx
@@ -1,5 +1,10 @@
+import { useEffect } from 'react';
 import './UsefulArticlesPage.scss';
 
 export const UsefulArticlesPage: React.FC = () => {
+  useEffect(() => {
+    document.title = 'Окуліст - корисне'
+  }, [])
+
   return <></>
 };
