37015e7c880038775bae8702075c0d53307895c0
Ros <traine41@icloud.com>
2024-02-13 14:33:57 +0000

    Minor fixes / improve code style / delete payment method switch to liqpay

--- back-end/src/main/java/spring/boot/optic/okulist/service/payment/WayForPayTransactionService.java
+++
@@ -1,8 +0,0 @@
-package spring.boot.optic.okulist.service.payment;
-
-import spring.boot.optic.okulist.dto.payment.TransactionListRequestDto;
-import spring.boot.optic.okulist.dto.payment.TransactionListResponseDto;
-
-public interface WayForPayTransactionService {
-    TransactionListResponseDto getTransactionList(TransactionListRequestDto requestDto);
-}
