16bda9a80d409531bf4834ca880bef30f3f0409a
Ros <traine41@icloud.com>
2024-03-05 15:11:48 +0000

    fix checkstyle

--- back-end/src/main/java/spring/boot/optic/okulist/config/HttpClientConfig.java
+++ back-end/src/main/java/spring/boot/optic/okulist/config/HttpClientConfig.java
@@ -1,12 +1,6 @@
 package spring.boot.optic.okulist.config;
 
-import java.net.http.HttpClient;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class HttpClientConfig {
--- back-end/src/main/java/spring/boot/optic/okulist/config/SecurityConfig.java
+++ back-end/src/main/java/spring/boot/optic/okulist/config/SecurityConfig.java
@@ -43,7 +43,6 @@ public class SecurityConfig {
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
-
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
