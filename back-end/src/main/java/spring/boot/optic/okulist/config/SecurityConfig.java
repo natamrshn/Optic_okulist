1e45b4de899ade03a27ec76f46aea7865432ce82
Ros <traine41@icloud.com>
2024-03-05 14:10:31 +0000

    cors

--- back-end/src/main/java/spring/boot/optic/okulist/config/SecurityConfig.java
+++ back-end/src/main/java/spring/boot/optic/okulist/config/SecurityConfig.java
@@ -17,7 +17,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import spring.boot.optic.okulist.security.JwtAuthenticationFilter;
+
+import java.util.List;
 
 @EnableMethodSecurity
 @EnableWebSecurity
@@ -33,8 +38,23 @@ public class SecurityConfig {
     }
 
     @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        // Replace "http://localhost:3000" with allowed origins from your front-end application
+        config.setAllowedOrigins(List.of("http://localhost:8080"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.cors(AbstractHttpConfigurer::disable)
+        return httpSecurity.cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**")
